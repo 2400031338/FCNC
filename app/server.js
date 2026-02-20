87770427562afd55b6c2eea953a4a5ad3654b763
BALA TIRUPATAIAH <gorlabala7@gmail.com>
2026-02-20 09:49:20 +0530

    added project folder

---
+++ app/server.js
@@ -0,0 +1,13 @@
+const express =require("express");  
+const app = express(); 
+// Home route 
+app.get("/", (req, res) => { 
+server.js 
+res.send("Hello! Node.js app is running using Docker Multi-Stage Build");
+});
+// Server port 
+const PORT = 3000; 
+// Start server 
+app.listen(PORT, () => { 
+console.log(`Server is running on port ${PORT}`); 
+});
